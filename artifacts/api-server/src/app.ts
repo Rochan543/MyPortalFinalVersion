b8a5b15d3794b0d14fa0fc64eca0a200ebca40fc
Rochan543 <premrochan@gmail.com>
2026-05-23 20:31:58 +0530

    add frontendurl

--- artifacts/api-server/src/app.ts
+++ artifacts/api-server/src/app.ts
@@ -25,7 +25,15 @@ app.use(
     },
   }),
 );
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      process.env.FRONTEND_URL!,
+    ],
+    credentials: true,
+  }),
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
