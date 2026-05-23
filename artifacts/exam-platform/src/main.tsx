f9388233a7fde24f168d9b49b2febad1fda1132a
Rochan543 <premrochan@gmail.com>
2026-05-23 20:20:51 +0530

    add VITE

--- artifacts/exam-platform/src/main.tsx
+++ artifacts/exam-platform/src/main.tsx
@@ -2,4 +2,8 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
+import { setBaseUrl } from "../../../lib/api-client-react/src/custom-fetch";
+
+setBaseUrl(import.meta.env.VITE_API_URL);
+
 createRoot(document.getElementById("root")!).render(<App />);
